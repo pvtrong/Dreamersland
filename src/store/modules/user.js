86b373257cc633f953b0b6ec5c62424c763d5738
Your Name <trong.pham@kiaisoft.com>
2023-10-20 10:12:06 +0700

    feat: integrate login

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -32,7 +32,8 @@ const actions = {
   login({ commit }, userInfo) {
     const { username, password } = userInfo
     return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password: password }).then(response => {
+      debugger
+      login({ email: username.trim(), password: password }).then(response => {
         const { data } = response
         commit('SET_TOKEN', data.token)
         setToken(data.token)
