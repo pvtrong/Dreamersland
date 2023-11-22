3dc9417b9b2e3910a0c12750a2cf4e47ff29a9ae
Pham Van Huy <huy.pham@bmbsoft.com.vn>
2023-11-22 14:42:04 +0700

    add option for calendar picker

--- src/views/rank/index.vue
+++ src/views/rank/index.vue
@@ -1,27 +1,48 @@
 <template>
   <article>
-    <breadcrumb :pageTitle="'Ranking'" :thumbImage="breadcrumbImage03" :isUserDetail="false"></breadcrumb>
+    <breadcrumb
+      :pageTitle="'Ranking'"
+      :thumbImage="breadcrumbImage03"
+      :isUserDetail="false"
+    ></breadcrumb>
 
     <section class="rank__list-area pb-[120px] pt-[120px] bg-center bg-cover">
       <div class="container md:px-[0rem] xsm:px-[1.5rem]">
         <custom-title :title="'Top of dreamers'"></custom-title>
 
-        <div class="sm:w-full sm:basis-full xsm:w-full xsm:basis-full relative lg:p-0 md:p-0 sm:p-0 xsm:p-0">
+        <div
+          class="sm:w-full sm:basis-full xsm:w-full xsm:basis-full relative lg:p-0 md:p-0 sm:p-0 xsm:p-0"
+        >
           <div class="shop__top-wrap mt-0 mb-[30px] mx-0">
             <div class="flex justify-end items-center">
               <div
-                class="shop__ordering flex gap-5 relative ml-auto after:content-['\f107'] after:absolute after:-translate-y-2/4 after:font-bold after:text-[14px] after:right-5 after:top-2/4 after:font-FontAwesome xsm:m-[15px_auto_0]">
-                <el-date-picker class="xsm:!w-full" v-model="filterBy" type="daterange" :ref="pickerRef" :picker-options="pickerOptions"
-                  @change="handleFilterBy">
-                  ></el-date-picker>
+                class="shop__ordering flex gap-5 relative ml-auto xsm:m-[15px_auto_0]"
+              >
+                <el-date-picker
+                  class="xsm:!w-full"
+                  v-model="filterBy"
+                  type="daterange"
+                  :ref="pickerRef"
+                  :picker-options="pickerOptions"
+                  @change="handleFilterBy"
+                >
+                  ></el-date-picker
+                >
               </div>
             </div>
           </div>
         </div>
         <div class="rank__wrapper py-0 xl:py-0 lg:p-0 md:p-0 sm:p-0 xsm:p-0">
-          <div class="flex flex-col md:gap-[1.5rem] sm:gap-[1.5rem] xsm:gap-[1.5rem]">
-            <rank-item v-for="(rank, index) in listRanks" :key="index" :index="index + 1" :rank="rank"
-              :animate="animateText"></rank-item>
+          <div
+            class="flex flex-col md:gap-[1.5rem] sm:gap-[1.5rem] xsm:gap-[1.5rem]"
+          >
+            <rank-item
+              v-for="(rank, index) in listRanks"
+              :key="index"
+              :index="index + 1"
+              :rank="rank"
+              :animate="animateText"
+            ></rank-item>
           </div>
         </div>
       </div>
@@ -40,11 +61,27 @@ import { RANK_SORT_BY, DATE_FORMAT } from '@/constants';
 import { getStartAndEndOfWeek, getStartAndEndOfMonth } from '@/utils';
 import moment from 'moment';
 
-const end = new Date();
-const start = new Date();
+const filterByDate = {
+  from: moment().format(DATE_FORMAT),
+  to: moment().format(DATE_FORMAT),
+};
 
-start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-const defaultDate = [start, end];
+const filterByWeek = {
+  from: moment(getStartAndEndOfWeek().startDate).format(DATE_FORMAT),
+  to: moment(getStartAndEndOfWeek().endDate).format(DATE_FORMAT),
+};
+
+const filterByCurrentMonth = {
+  from: moment(getStartAndEndOfMonth().startDate).format(DATE_FORMAT),
+  to: moment(getStartAndEndOfMonth().endDate).format(DATE_FORMAT),
+};
+
+const filterByPreviousMonth = {
+  from: moment(getStartAndEndOfMonth(1).startDate).format(DATE_FORMAT),
+  to: moment(getStartAndEndOfMonth(1).endDate).format(DATE_FORMAT),
+};
+
+const defaultDate = [filterByCurrentMonth.from, filterByCurrentMonth.to];
 
 export default {
   name: 'Bảng xếp hạng',
@@ -55,10 +92,6 @@ export default {
     CustomTitle,
   },
   data() {
-    const filterDate = new Date();
-    const filterTime = {
-      date: filterDate,
-    };
     return {
       isLoading: false,
       typeDate: 'daterange',
@@ -91,21 +124,24 @@ export default {
             },
           },
           {
-            text: '7 ngày trước',
+            text: 'Tuần trước',
             onClick(picker) {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-              picker.$emit('pick', [start, end]);
+              const { from, to } = filterByWeek;
+              picker.$emit('pick', [from, to]);
             },
           },
           {
-            text: '30 ngày trước',
+            text: 'Tháng này',
             onClick(picker) {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-              picker.$emit('pick', [start, end]);
+              const { from, to } = filterByCurrentMonth;
+              picker.$emit('pick', [from, to]);
+            },
+          },
+          {
+            text: 'Tháng trước',
+            onClick(picker) {
+              const { from, to } = filterByPreviousMonth;
+              picker.$emit('pick', [from, to]);
             },
           },
         ],
@@ -129,7 +165,7 @@ export default {
       try {
         const { data } = await getListRanks({ ...params });
         this.listRanks = data;
-      } catch (error) { }
+      } catch (error) {}
       this.isLoading = false;
     },
 
@@ -138,7 +174,7 @@ export default {
         const { data } = await getListSeasons();
         this.seasonOptions = data;
         this.selectedSeason = data[0] ?? null;
-      } catch (error) { }
+      } catch (error) {}
     },
 
     handleFilterBy(value) {
@@ -158,7 +194,7 @@ export default {
     changeType(newType) {
       this.typeDate = value;
     },
-    exploreMore() { },
+    exploreMore() {},
     openDateRangePicker() {
       // Access the date range picker using its ref
       const datePicker = this.$refs.datePickerRef;
@@ -177,6 +213,6 @@ export default {
     setTimeout(() => {
       main(window.jQuery);
     });
-  }
+  },
 };
 </script>
